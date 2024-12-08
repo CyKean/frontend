e338c2f462e6334f5d78e8ccf2429092f43aff78
PerjesCy <perjescykean356@gmail.com>
2024-12-08 18:41:43 +0800

    update

--- src/components/org/OrganizationalChart.vue
+++ src/components/org/OrganizationalChart.vue
@@ -1,305 +1,245 @@
 <template>
-    <div class="container">
-      <div class="button-group">
-        <button @click="addRootNode" class="bg-yellow-200">Add Initial Node</button>
-        <button @click="logChartData" class="bg-green-500 text-green-100">Save Chart</button>
-        <button @click="show" class="bg-green-300 text-green-600">show</button>
-      </div>
-      <div id="chartDiv" class="chart rounded-md border border-green-200"></div>
-  
-      <div v-if="isModalVisible" class="modal-overlay z-10" @click.self="closeModal">
-        <div class="modal">
-          <h3>Edit Node</h3>
-          <label for="name">Name:</label>
-          <input id="name" v-model="editNodeData.name" placeholder="Enter name" />
-          <label for="bgColor">Background Color:</label>
-          <input id="bgColor" type="color" v-model="editNodeData.color" />
-          <label for="fontColor">Font Color:</label>
-          <input id="fontColor" type="color" v-model="editNodeData.fontColor" />
-          <div class="modal-buttons">
-            <button @click="saveEditNode" class="btn-primary">Save</button>
-            <button @click="closeModal" class="btn-secondary">Cancel</button>
-          </div>
+  <div class="container">
+    <div class="button-group">
+      <button @click="addRootNode" class="bg-yellow-200">Add Initial Node</button>
+      <button @click="logChartData" class="bg-green-500 text-green-100">Save Chart</button>
+    </div>
+    <div id="chartDiv" class="chart rounded-md border border-green-200" style="width: 100%; height: 500px;"></div>
+
+    <div v-if="isModalVisible" class="modal-overlay z-10" @click.self="closeModal">
+      <div class="modal">
+        <h3>Edit Node</h3>
+        <label for="name">Name:</label>
+        <input id="name" v-model="editNodeData.name" placeholder="Enter name" />
+        <label for="bgColor">Background Color:</label>
+        <input id="bgColor" type="color" v-model="editNodeData.color" />
+        <label for="fontColor">Font Color:</label>
+        <input id="fontColor" type="color" v-model="editNodeData.fontColor" />
+        <div class="modal-buttons">
+          <button @click="saveEditNode" class="btn-primary">Save</button>
+          <button @click="closeModal" class="btn-secondary">Cancel</button>
         </div>
       </div>
-  
-      <vue-toastr ref="toastr" />
     </div>
-  </template>
-  
+
+    <vue-toastr ref="toastr" />
+  </div>
+</template>
+
 <script>
-  import * as go from "gojs";
-  import toastr from "toastr";
-  import axios from "axios";
-  
-  export default {
-    data() {
-      return {
-        diagram: null,
-        nodeDataArray: [],
-        rootNodeKey: 1,
-        selectedNode: null,
-        isModalVisible: false,
-        editNodeData: { name: "", color: "#FFFFFF", fontColor: "#000000" },
-      };
-    },
-    // mounted() {
-    //   const savedData = localStorage.getItem("orgChartData");
-    //   console.log(savedData);
-    //   if (savedData) {
-    //     this.nodeDataArray = JSON.parse(savedData);
-    //   }
-  
-    //   this.diagram = go.GraphObject.make(go.Diagram, "chartDiv", {
-    //     layout: go.GraphObject.make(go.TreeLayout, {
-    //       angle: 90,
-    //       layerSpacing: 35,
-    //     }),
-    //     initialAutoScale: go.Diagram.UniformToFill,
-    //     "undoManager.isEnabled": true,
-    //     "click": (e) => this.handleDiagramClick(e),
-    //   });
-  
-    //   this.diagram.nodeTemplate = go.GraphObject.make(
-    //     go.Node,
-    //     "Auto",
-    //     go.GraphObject.make(go.Shape, "RoundedRectangle", {
-    //       stroke: null,
-    //       portId: "",
-    //       fromLinkable: true,
-    //       toLinkable: true,
-    //       figure: "RoundedRectangle",
-    //     }, new go.Binding("fill", "color")),
-        
-    //     go.GraphObject.make(
-    //       go.Panel,
-    //       "Table",
-    //       { margin: 5 },
-    //       go.GraphObject.make(go.TextBlock, {
-    //         row: 0,
-    //         font: "bold 12pt sans-serif",
-    //         margin: 2,
-    //       }, new go.Binding("text", "name"),
-    //       new go.Binding("stroke", "fontColor"))
-    //     ),
-        
-    //     go.GraphObject.make(go.Panel, "Auto", {
-    //       alignment: go.Spot.TopRight,
-    //       visible: false, // Initially hidden
-    //       name: "CHECK_ICON_PANEL",
-    //     },
-    //       go.GraphObject.make(go.TextBlock, {
-    //         font: "bold 16px sans-serif",
-    //         stroke: "green", // Icon color
-    //       }, new go.Binding("text", "", (node) => (this.selectedNode === node ? "✔" : "")))
-    //     ),
-        
-    //     {
-    //       contextMenu: this.makeContextMenu(),
-    //       click: (e, node) => this.handleNodeClick(node),
-    //     }
-    //   );
-  
-    //   // Update linkTemplate to include curve
-    //   this.diagram.linkTemplate = go.GraphObject.make(go.Link,
-    //     go.GraphObject.make(go.Shape, {
-    //       strokeWidth: 2, 
-    //       stroke: "#000", 
-    //     //   curve: go.Link.Bezier  // Define curve for links
-    //     }),
-    //     go.GraphObject.make(go.Shape, { toArrow: "Standard" }) // Optional: add arrow
-    //   );
-  
-    //   // Set model data (either from localStorage or initial state)
-    //   this.diagram.model = new go.TreeModel(this.nodeDataArray);
-    //   this.diagram.requestUpdate();
-    // },
+import * as go from "gojs";
+import toastr from "toastr";
+import axios from "axios";
 
-    mounted() {
-        const savedData = localStorage.getItem("orgChartData");
-        if (savedData) {
-            this.nodeDataArray = JSON.parse(savedData);
-        }
+export default {
+  data() {
+    return {
+      diagram: null,
+      nodeDataArray: [],
+      rootNodeKey: 1,
+      selectedNode: null,
+      isModalVisible: false,
+      editNodeData: { name: "", color: "#FFFFFF", fontColor: "#000000" },
+    };
+  },
 
-        this.diagram = go.GraphObject.make(go.Diagram, "chartDiv", {
-            layout: go.GraphObject.make(go.TreeLayout, {
-            angle: 90,
-            layerSpacing: 35,
-            }),
-            initialAutoScale: go.Diagram.UniformToFill,
-            "undoManager.isEnabled": true,
-            "click": (e) => this.handleDiagramClick(e),
-        });
+  mounted() {
+    // Initialize the GoJS Diagram
+    this.diagram = go.GraphObject.make(go.Diagram, "chartDiv", {
+      layout: go.GraphObject.make(go.TreeLayout, {
+        angle: 90,
+        layerSpacing: 35,
+      }),
+      initialAutoScale: go.Diagram.UniformToFill,
+      "undoManager.isEnabled": true,
+      "click": (e) => this.handleDiagramClick(e),
+    });
 
-        this.diagram.nodeTemplate = go.GraphObject.make(
-            go.Node,
-            "Auto",
-            go.GraphObject.make(go.Shape, "RoundedRectangle", {
-            stroke: null,
-            portId: "",
-            fromLinkable: true,
-            toLinkable: true,
-            }, new go.Binding("fill", "color")),
-            go.GraphObject.make(
-            go.Panel,
-            "Table",
-            { margin: 5 },
-            go.GraphObject.make(go.TextBlock, {
-                row: 0,
-                font: "bold 12pt sans-serif",
-                margin: 2,
-            }, new go.Binding("text", "name"),
-                new go.Binding("stroke", "fontColor"))
-            ),
-            {
-            contextMenu: this.makeContextMenu(),
-            click: (e, node) => this.handleNodeClick(node),
-            }
-        );
+    this.diagram.nodeTemplate = go.GraphObject.make(
+      go.Node,
+      "Auto",
+      go.GraphObject.make(go.Shape, "RoundedRectangle", {
+        stroke: null,
+        portId: "",
+        fromLinkable: true,
+        toLinkable: true,
+      }, new go.Binding("fill", "color")),
+      go.GraphObject.make(
+        go.Panel,
+        "Table",
+        { margin: 5 },
+        go.GraphObject.make(go.TextBlock, {
+          row: 0,
+          font: "bold 12pt sans-serif",
+          margin: 2,
+        }, new go.Binding("text", "name"),
+          new go.Binding("stroke", "fontColor"))
+      ),
+      {
+        contextMenu: this.makeContextMenu(),
+        click: (e, node) => this.handleNodeClick(node),
+      }
+    );
 
-        this.diagram.linkTemplate = go.GraphObject.make(go.Link,
-            go.GraphObject.make(go.Shape, { strokeWidth: 2, stroke: "#000" }),
-            go.GraphObject.make(go.Shape, { toArrow: "Standard" })
-        );
+    this.diagram.linkTemplate = go.GraphObject.make(go.Link,
+      go.GraphObject.make(go.Shape, { strokeWidth: 2, stroke: "#000" }),
+      go.GraphObject.make(go.Shape, { toArrow: "Standard" })
+    );
 
-        // Initialize model
-        this.diagram.model = new go.TreeModel(this.nodeDataArray);
-        this.$nextTick(() => {
-            this.show();
-        });
-    },
+    this.fetchChartData();
+  },
 
-    methods: {
-      addRootNode() {
-        if (this.nodeDataArray.length > 0) {
-          toastr.warning("Root node already exists.");
-          return;
-        }
-        const rootNode = {
-          key: this.rootNodeKey,
-          name: "CEO",
-          color: "lightblue",
-          fontColor: "#000000",
-        };
-        this.nodeDataArray.push(rootNode);
-        this.diagram.model = new go.TreeModel(this.nodeDataArray);
-        toastr.success("Root node added successfully.");
-      },
-  
-      addNode(parentNodeData) {
-        const newKey = this.nodeDataArray.length + 1;
-        const newNode = {
-          key: newKey,
-          parent: parentNodeData ? parentNodeData.key : null,  // Link to the parent if available
-          name: `New Node ${newKey}`,
-          color: "lightblue",
-          fontColor: "#000000",
-        };
-  
-        this.nodeDataArray.push(newNode);
-        this.diagram.model = new go.TreeModel(this.nodeDataArray);
-        toastr.success("Node added successfully.");
-      },
+  methods: {
+    async fetchChartData() {
+      try {
+        const response = await axios.get("https://backend-w85m.onrender.com/api/get-chart");
 
-      show(){
-        // console.log("Saving chart data", this.nodeDataArray);
-        // localStorage.getItem("orgChartData", JSON.stringify(this.nodeDataArray));
-        // // toastr.success("Chart data saved to local storage.");
+        if (response.data && response.data.nodes) {
+          const fetchedData = response.data.nodes;
 
-        this.diagram.model = new go.TreeModel(this.nodeDataArray);
-      },
-  
-      logChartData() {
-        console.log("Current Chart Data:", this.nodeDataArray);
-  
-        localStorage.setItem("orgChartData", JSON.stringify(this.nodeDataArray));
-        // toastr.success("Chart data saved to local storage.");
-
-        // this.diagram.model = new go.TreeModel(this.nodeDataArray);
-  
-        axios.post('https://backend-w85m.onrender.com/api/save-chart', this.nodeDataArray)
-          .then(response => {
-            toastr.success(response.data.message);
-          })
-          .catch(error => {
-            toastr.error("Failed to save chart data to Firebase.");
+          this.nodeDataArray = fetchedData.map((node, index) => {
+            return {
+              ...node,
+              parent: index === 0 ? undefined : 1 // The first node (CEO) has no parent, others are children of node with key 1
+            };
           });
-      },
-  
-      editNode(nodeData) {
-        this.editNodeData = { ...nodeData };
-        this.isModalVisible = true;
-      },
-  
-      handleNodeClick(node) {
-        if (this.selectedNode === node) {
-          this.selectedNode = null;
-        } else {
-          this.selectedNode = node;
-        }
-      },
-  
-      handleDiagramClick() {
-        this.selectedNode = null;
-      },
-  
-      makeContextMenu() {
-        return go.GraphObject.make(go.Adornment, "Vertical",
-          go.GraphObject.make(go.TextBlock, {
-            text: "Edit Node",
-            click: (e, obj) => {
-              const nodeData = obj.part.data;
-              this.editNode(nodeData);
-            }
-          }),
-          go.GraphObject.make(go.TextBlock, {
-            text: "Delete Node",
-            click: (e, obj) => {
-              const nodeData = obj.part.data;
-              this.deleteNode(nodeData);
-            }
-          }),
-          go.GraphObject.make(go.TextBlock, {
-            text: "Add Node",  // The new menu item
-            click: (e, obj) => {
-              const nodeData = obj.part.data; // Optionally use the selected node data for parent-child relationships
-              this.addNode(nodeData);
-            }
-          })
-        );
-      },
-  
-      deleteNode(nodeData) {
-        if (nodeData.key === this.rootNodeKey) {
-          toastr.error("Cannot delete the root node.");
-          return;
-        }
-        this.diagram.startTransaction("Delete Node");
-        const model = this.diagram.model;
-        model.removeNodeData(nodeData);
-        this.diagram.commitTransaction("Delete Node");
-        this.diagram.model = new go.TreeModel(this.nodeDataArray);
-        toastr.success("Node deleted successfully.");
-      },
-  
-      saveEditNode() {
-        const updatedNode = this.nodeDataArray.find(node => node.key === this.editNodeData.key);
-        if (updatedNode) {
-          updatedNode.name = this.editNodeData.name;
-          updatedNode.color = this.editNodeData.color;
-          updatedNode.fontColor = this.editNodeData.fontColor;
+
+          localStorage.setItem("orgChartData", JSON.stringify(this.nodeDataArray));
+
           this.diagram.model = new go.TreeModel(this.nodeDataArray);
-          toastr.success("Node updated successfully.");
-          this.closeModal();
+        } else {
+          console.error("No valid data found from the API.");
         }
-      },
-  
-      closeModal() {
-        this.isModalVisible = false;
-      },
+      } catch (error) {
+        console.error("Error fetching chart data:", error);
+      }
     },
-  };
-  </script>
+
+    addRootNode() {
+      if (this.nodeDataArray.length > 0) {
+        toastr.warning("Root node already exists.");
+        return;
+      }
+      const rootNode = {
+        key: this.rootNodeKey,
+        name: "CEO",
+        color: "lightblue",
+        fontColor: "#000000",
+      };
+      this.nodeDataArray.push(rootNode);
+      this.show();
+      toastr.success("Root node added successfully.");
+    },
+
+    addNode(parentNodeData) {
+      const newKey = this.nodeDataArray.length + 1;
+      const newNode = {
+        key: newKey,
+        parent: parentNodeData ? parentNodeData.key : null,  // Link to the parent if available
+        name: `New Node ${newKey}`,
+        color: "lightblue",
+        fontColor: "#000000",
+      };
+
+      this.nodeDataArray.push(newNode);
+      this.show();
+      toastr.success("Node added successfully.");
+    },
+
+    show() {
+      const model = new go.TreeModel(this.nodeDataArray);
+      this.diagram.model = model;
+    },
+
+    logChartData() {
+      console.log("Current Chart Data:", this.nodeDataArray);
+
+      localStorage.setItem("orgChartData", JSON.stringify(this.nodeDataArray));
+
+      axios.post('http://localhost:3000/api/save-chart', this.nodeDataArray)
+        .then(response => {
+          toastr.success(response.data.message);
+        })
+        .catch(error => {
+          toastr.error("Failed to save chart data.");
+        });
+    },
+
+    editNode(nodeData) {
+      this.editNodeData = { ...nodeData };
+      this.isModalVisible = true;
+    },
+
+    handleNodeClick(node) {
+      if (this.selectedNode === node) {
+        this.selectedNode = null;
+      } else {
+        this.selectedNode = node;
+      }
+    },
+
+    handleDiagramClick() {
+      this.selectedNode = null;
+    },
+
+    makeContextMenu() {
+      return go.GraphObject.make(go.Adornment, "Vertical",
+        go.GraphObject.make(go.TextBlock, {
+          text: "Edit Node",
+          click: (e, obj) => {
+            const nodeData = obj.part.data;
+            this.editNode(nodeData);
+          }
+        }),
+        go.GraphObject.make(go.TextBlock, {
+          text: "Delete Node",
+          click: (e, obj) => {
+            const nodeData = obj.part.data;
+            this.deleteNode(nodeData);
+          }
+        }),
+        go.GraphObject.make(go.TextBlock, {
+          text: "Add Node",
+          click: (e, obj) => {
+            const nodeData = obj.part.data;
+            this.addNode(nodeData);
+          }
+        })
+      );
+    },
+
+    deleteNode(nodeData) {
+      if (nodeData.key === this.rootNodeKey) {
+        toastr.error("Cannot delete the root node.");
+        return;
+      }
+      this.diagram.startTransaction("Delete Node");
+      const model = this.diagram.model;
+      model.removeNodeData(nodeData);
+      this.diagram.commitTransaction("Delete Node");
+      this.show();
+      toastr.success("Node deleted successfully.");
+    },
+
+    saveEditNode() {
+      const updatedNode = this.nodeDataArray.find(node => node.key === this.editNodeData.key);
+      if (updatedNode) {
+        updatedNode.name = this.editNodeData.name;
+        updatedNode.color = this.editNodeData.color;
+        updatedNode.fontColor = this.editNodeData.fontColor;
+        this.show();
+        toastr.success("Node updated successfully.");
+        this.closeModal();
+      }
+    },
+
+    closeModal() {
+      this.isModalVisible = false;
+    },
+  },
+};
+</script>
+
   
   <style scoped>
   .container {
--- src/views/OrgChart.vue
+++ src/views/OrgChart.vue
@@ -4,10 +4,6 @@
 
     <div class="p-4 flex justify-center space-x-2">
       <div id="chart" class="chart border border-green-200 rounded-md"></div>
-      <div>
-        <button @click="show" class="bg-green-500 py-1 px-3 rounded-md text-green-100 hover:bg-green-400">Show Chart</button>
-      </div>
-     
     </div>
     <Footer/>
   </div>
@@ -29,6 +25,7 @@ export default {
     };
   },
   mounted() {
+    this.fetchChartData(); // Fetch data on mount
     const savedData = localStorage.getItem("orgChartData");
     if (savedData) {
       this.nodeDataArray = JSON.parse(savedData);
@@ -48,67 +45,39 @@ export default {
         return;
       }
 
-      this.diagram = $(
-        go.Diagram,
-        chartContainer, // Reference to the div container
-        {
-          layout: $(
-            go.TreeLayout,
-            {
-              angle: 90,
-              layerSpacing: 35,
-            }
-          ),
-          initialAutoScale: go.Diagram.UniformToFill,
-          "undoManager.isEnabled": true,
-          isReadOnly: true,
-        }
-      );
+      this.diagram = $(go.Diagram, chartContainer, {
+        layout: $(go.TreeLayout, {
+          angle: 90,
+          layerSpacing: 35,
+        }),
+        initialAutoScale: go.Diagram.UniformToFill,
+        "undoManager.isEnabled": true,
+        isReadOnly: true,
+      });
 
-      this.diagram.nodeTemplate = $(
-        go.Node,
-        "Auto",
-        {
-          movable: false, // Disables moving nodes
-        },
-        $(
-          go.Shape,
-          "RoundedRectangle",
-          {
-            stroke: null,
-            portId: "",
-            fromLinkable: true,
-            toLinkable: true,
-          },
-          new go.Binding("fill", "color")
-        ),
-        $(
-          go.Panel,
-          "Table",
-          { margin: 5 },
-          $(
-            go.TextBlock,
-            {
-              row: 0,
-              font: "bold 12pt sans-serif",
-              margin: 2,
-            },
-            new go.Binding("text", "name"),
-            new go.Binding("stroke", "fontColor")
-          )
-        )
-      );
+      // Define the node template
+      this.diagram.nodeTemplate = $(go.Node, "Auto", {
+        movable: false, // Disable moving nodes
+      },
+      $(go.Shape, "RoundedRectangle", {
+        stroke: null,
+        portId: "",
+        fromLinkable: true,
+        toLinkable: true,
+      },
+      new go.Binding("fill", "color")),
+      $(go.Panel, "Table", { margin: 5 },
+        $(go.TextBlock, {
+          row: 0,
+          font: "bold 12pt sans-serif",
+          margin: 2,
+        }, new go.Binding("text", "name"), new go.Binding("stroke", "fontColor"))
+      ));
 
-      this.diagram.linkTemplate = $(
-        go.Link,
-        $(
-          go.Shape,
-          { strokeWidth: 2, stroke: "#000" } // Link line
-        ),
-        $(
-          go.Shape,
-          { toArrow: "Standard" } // Arrowhead
-        )
+      // Define the link template
+      this.diagram.linkTemplate = $(go.Link,
+        $(go.Shape, { strokeWidth: 2, stroke: "#000" }), // Link line
+        $(go.Shape, { toArrow: "Standard" }) // Arrowhead
       );
 
       this.diagram.model = new go.TreeModel(this.nodeDataArray);
@@ -117,10 +86,24 @@ export default {
       try {
         const response = await axios.get("https://backend-w85m.onrender.com/api/get-chart");
 
-        this.nodeDataArray = response.data || [];
-        localStorage.setItem("orgChartData", JSON.stringify(this.nodeDataArray));
+        // If data is fetched, set it and store it in localStorage
+        if (response.data && response.data.nodes) {
+          const fetchedData = response.data.nodes;
 
-        this.diagram.model = new go.TreeModel(this.nodeDataArray);
+          // Ensure each node has the parent field to build a hierarchical structure
+          this.nodeDataArray = fetchedData.map((node, index) => {
+            return {
+              ...node,
+              parent: index === 0 ? undefined : 1 // The first node (CEO) has no parent, others are children of node with key 1
+            };
+          });
+
+          localStorage.setItem("orgChartData", JSON.stringify(this.nodeDataArray));
+
+          this.diagram.model = new go.TreeModel(this.nodeDataArray);
+        } else {
+          console.error("No valid data found from the API.");
+        }
       } catch (error) {
         console.error("Error fetching chart data:", error);
       }
@@ -132,14 +115,11 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .chart {
   width: 70rem;
   height: 600px;
   border: 1px solid #ccc;
   margin-bottom: 20px;
-}
-.wc{
-  width: 30rem;
 }
 </style>
